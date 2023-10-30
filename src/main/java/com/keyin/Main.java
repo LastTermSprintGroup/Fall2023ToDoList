85efb126f39ff0e14b231276efc6159293a37ac0
TylerSGFW <117922260+TylerSGFW@users.noreply.github.com>
2023-10-30 18:01:28 -0230

    Main file complete

--- src/main/java/com/keyin/Main.java
+++ src/main/java/com/keyin/Main.java
@@ -1,4 +1,26 @@
 package com.keyin;
 
 public class Main {
+    public static void main(String[] args) {
+        // 1. Creates users and adds them to an array.
+        User[] users = new User[2];
+        users[0] = new User("Alice");
+        users[1] = new User("Bob");
+
+        // 2. Adds tasks to user’s to-do lists.
+        users[0].addTask("Buy groceries");
+        users[0].addTask("Go to the gym");
+        users[1].addTask("Finish the report");
+        users[1].addTask("Attend the meeting");
+
+        // 3. Marks tasks as completed.
+        users[0].markTaskAsCompleted("Buy groceries");
+        users[1].markTaskAsCompleted("Attend the meeting");
+
+        // 4. Prints all tasks for each user.
+        for (User user : users) {
+            user.printAllTasks();
+            System.out.println();  // Just to separate the lists for clarity
+        }
+    }
 }
